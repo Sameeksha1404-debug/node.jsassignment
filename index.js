40b1ede07eeec082fb7424d30231359e6fb78f34
Sameeksha <sameeksha140404@gmail.com>
2024-08-10 23:11:10 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const calculate=require('./calculator')
+//calculator get an object
+console.log(calculate.add(5,6))
+console.log(calculate.minus(10,6))
+console.log(calculate.mul(5,6))
+console.log(calculate.div(56,6))
+
+/**output
+ 11
+4
+30
+9.333333333333334
+ */
