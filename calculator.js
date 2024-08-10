e264b3f8c37315f34c0be088b6e9a7bda031c1a1
Sameeksha <sameeksha140404@gmail.com>
2024-08-10 23:10:35 +0530

    Update calculator.js

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const add=(a,b)=>{
+    return a+b
+}
+
+const minus=(a,b)=>{
+    return a-b
+}
+
+const mul=(a,b)=>{
+    return a*b
+}
+
+const div=(a,b)=>{
+    return a/b
+}
+module.exports={
+    add:add,
+    minus:minus,
+    mul:mul,
+    div:div
+}
